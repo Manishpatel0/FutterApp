6fe624be2867ced4e729f092254691d6251bbfa6
Manishpatel0 <mp0000091@gmail.com>
2023-01-23 14:00:52 +0530

    Demo line of web

---
+++ DartProject/hello/bin/hello.dart
@@ -0,0 +1,9 @@
+import 'package:hello/hello.dart' as hello;
+import 'dart:math';
+
+void main(List<String> arguments) {
+  print('Hello');
+  for (var i = 0; i < 11; i++) {
+    print("Hello ${i}");
+  }
+}
