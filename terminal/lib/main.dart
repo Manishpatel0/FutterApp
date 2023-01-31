9156fff2cff253564a9f1dc4da6ae49950f1e7dd
Manishpatel0 <mp0000091@gmail.com>
2023-01-31 23:46:06 +0530

    Terminal UI

---
+++ terminal/lib/main.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:pty/pty.dart';
+//import 'package:example/src/platform_menu.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+//import 'package:flutter_acrylic/flutter_acrylic.dart';
+//import 'package:flutter_pty/flutter_pty.dart';
+import 'package:xterm/xterm.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'xterm.dart demo',
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      // shortcuts: ,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final terminal = Terminal(
+    maxLines: 10000,
+  );
+
+  final terminalController = TerminalController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: SafeArea(
+        child: TerminalView(
+          terminal,
+          controller: terminalController,
+          autofocus: true,
+          backgroundOpacity: 0.7,
+          onSecondaryTapDown: (details, offset) async {
+            final selection = terminalController.selection;
+            if (selection != null) {
+              final text = terminal.buffer.getText(selection);
+              terminalController.clearSelection();
+              await Clipboard.setData(ClipboardData(text: text));
+            } else {
+              final data = await Clipboard.getData('text/plain');
+              final text = data?.text;
+              if (text != null) {
+                terminal.paste(text);
+              }
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+String get shell {
+  if (Platform.isMacOS || Platform.isLinux) {
+    return Platform.environment['SHELL'] ?? 'bash';
+  }
+
+  if (Platform.isWindows) {
+    return 'cmd.exe';
+  }
+
+  return 'sh';
+}
