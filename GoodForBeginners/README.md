e9a5cbc140bdb75391dd884bdebecf4662983a18
manishpatel0 <mp0000091@gmail.com>
2023-11-04 18:24:00 +0530

    Hello

--- GoodForBeginners/README.md
+++
@@ -1,3 +0,0 @@
-# Good for Beginners 
-[] Your first Flutter app 
-[] Write your first Flutter app on the web 
--- android/app/src/main/kotlin/com/nandani/hindityping/hindityping/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.nandani.hindityping.hindityping
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
